72f51d38b3f770ebc5cfdcf9963c664f53de0b47
LWiscool7 <93746258+LWiscool7@users.noreply.github.com>
2024-03-22 10:43:09 -0500

    intake comments

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -18,38 +18,54 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
 
+  //intake Talon
   TalonFX m_intake = new TalonFX(IntakeConstants.INTAKE);
+
+  //photoelectric sensor, with DIO port
   DigitalInput m_photoelectric = new DigitalInput(IntakeConstants.PHOTOELECTRIC_DIO);
 
-  /** Creates a new Intake. */
+  /* Creates a new Intake.
+  Runs on robot Init
+  */
   public Intake() {
+    //Applys motor config defaults for the Intake
     TalonUtils.ApplyRunMotorSlot(m_intake);
   }
 
+  //Local PID set command. The intake only sets PID to its current position, so it needs no parameters
   private void SetMotorPID() {
     TalonUtils.TalonPIDControl(m_intake, m_intake.getPosition().getValue());
   }
 
-  public void IntakeForward() {
-    TalonUtils.TalonVelocityControl(m_intake, 0.3);
-  }
 
+  //Intake max speed, runs with shooting motor
   public void IntakeMax() {
     TalonUtils.TalonVelocityControl(m_intake, 1);
   }
 
+  //Intake Pickup notes
+  public void IntakeForward() {
+    TalonUtils.TalonVelocityControl(m_intake, 0.3);
+  }
+
+  //turns the intake off, used specifically after shooting notes
   public void IntakeOff() {
     TalonUtils.TalonVelocityControl(m_intake, 0);
   }
 
-  public void IntakePIDOff() {
-    SetMotorPID();
-  }
-
+  //backup, reverses intake. runs in parrallel with shooter reverse to reposition notes
   public void IntakeReverse() {
     TalonUtils.TalonVelocityControl(m_intake, -0.2);
   }
 
+
+  //this command sets the motors to go to its current PID position
+  //it is used when intaking notes, and will set to the position when the photoelectric sensor goes off. 
+  public void IntakePIDOff() {
+    SetMotorPID();
+  }
+
+  //used to detect when the photoelectric sensor sees a note
   public boolean GetSwitchHit() {
     return m_photoelectric.get();
   }
@@ -58,6 +74,6 @@ public class Intake extends SubsystemBase {
   public void periodic() {
     SmartDashboard.putBoolean("Photoelectric Sensor Value", m_photoelectric.get());
 
-    // This method will be called once per scheduler run
+    // This method will be called once per scheduler run (every 20ms)
   }
 }
